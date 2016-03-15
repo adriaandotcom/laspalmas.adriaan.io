e666a3dfe121784407a613f2d467c36dff449055
Adriaan <harianus@users.noreply.github.com>
2016-03-15 23:02:20 +0100

    Reset email field and give response field the block styling

--- js/app.js
+++ js/app.js
@@ -4,15 +4,19 @@ document.querySelector('form.newsletter').addEventListener('submit', function(ev
   event.preventDefault();
   
   // Get email
-  var email = document.querySelector('form.newsletter input[name="email"]').value
+  var email = document.querySelector('form.newsletter input[name="email"]')
   
   // Send newsletter email to myself
-  // var url = 'https://screenmessage.com/blog/mail.php?subject=New+subscription+for+Las+Palmas+blog&message=Email+address:+' + email;
-  // var request = new XMLHttpRequest();
-  // request.open('GET', url, true);
-  // request.send();
+  var url = 'https://screenmessage.com/blog/mail.php?subject=New+subscription+for+Las+Palmas+blog&message=Email+address:+' + email.value;
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.send();
   
+  // Show response
   var responseField = document.querySelector('form.newsletter p.response');
-  responseField.style.display = 'initial';
+  responseField.style.display = 'block';
+  
+  // Reset email field
+  email.value = '';
 })
 
