10bc0d0922051b8ec70d550c50738da8661e1a92
Adriaan <harianus@users.noreply.github.com>
2016-03-15 23:37:48 +0100

    Add amount of people logic

--- js/app.js
+++ js/app.js
@@ -1,22 +1,38 @@
-document.querySelector('form.newsletter').addEventListener('submit', function(event) {
 
-  // Don't do anything, browser!
-  event.preventDefault();
+var newsLetterForm = document.querySelector('form.newsletter');
+
+if (newsLetterForm) {
   
-  // Get email
-  var email = document.querySelector('form.newsletter input[name="email"]')
+  // Fake the amount of people, who cares?
+  var amoundField = newsLetterForm.querySelector('.amount-of-people');
   
-  // Send newsletter email to myself
-  var url = 'https://screenmessage.com/blog/mail.php?subject=New+subscription+for+Las+Palmas+blog&message=Email+address:+' + email.value;
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.send();
+  var started = 1458081344871;
+  var now = (new Date).getTime();
+  var total = (now - started) / 1000 / 60 / 60 / 24 + 10;
+  var amount = Math.round(total);
   
-  // Show response
-  var responseField = document.querySelector('form.newsletter p.response');
-  responseField.style.display = 'block';
+  amoundField.innerHTML = amount;
+
+  newsLetterForm.addEventListener('submit', function(event) {
   
-  // Reset email field
-  email.value = '';
-})
+    // Don't do anything, browser!
+    event.preventDefault();
+    
+    // Get email
+    var email = newsLetterForm.querySelector('input[name="email"]')
+    
+    // Send newsletter email to myself
+    var url = 'https://screenmessage.com/blog/mail.php?subject=New+subscription+for+Las+Palmas+blog&message=Email+address:+' + email.value;
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.send();
+    
+    // Show response
+    var responseField = newsLetterForm.querySelector('.response');
+    responseField.style.display = 'block';
+    
+    // Reset email field
+    email.value = '';
+  })
+}
 
