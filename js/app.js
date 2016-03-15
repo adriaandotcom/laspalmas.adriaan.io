9f6d81ee3a30d35b95acbaeec03c07eb096737a3
Adriaan <harianus@users.noreply.github.com>
2016-03-15 22:33:28 +0100

    Add functionality to add people to the newsletter

---
+++ js/app.js
@@ -0,0 +1,15 @@
+document.querySelector('form.newsletter').addEventListener('submit', function(event) {
+
+  // Don't do anything, browser!
+  event.preventDefault();
+  
+  // Get email
+  var email = document.querySelector('form.newsletter name["email"]').value;
+  
+  // Send newsletter email to myself
+  var url = 'http://watiseropderadio.nl/tools/mail?subject=New+subscriber+Las+Palmas+blog:+' + email;
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.send();
+})
+
