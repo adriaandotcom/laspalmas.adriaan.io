ed82553d28c88d248e0f07a0338bd3cbfdded8ee
Adriaan <harianus@users.noreply.github.com>
2016-03-15 22:59:24 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,9 +7,12 @@ document.querySelector('form.newsletter').addEventListener('submit', function(ev
   var email = document.querySelector('form.newsletter input[name="email"]').value
   
   // Send newsletter email to myself
-  var url = 'https://screenmessage.com/blog/mail.php?subject=New+subscription+for+Las+Palmas+blog&message=Email+address:+' + email;
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.send();
+  // var url = 'https://screenmessage.com/blog/mail.php?subject=New+subscription+for+Las+Palmas+blog&message=Email+address:+' + email;
+  // var request = new XMLHttpRequest();
+  // request.open('GET', url, true);
+  // request.send();
+  
+  var responseField = document.querySelector('form.newsletter p.response');
+  responseField.style.display = 'initial';
 })
 
