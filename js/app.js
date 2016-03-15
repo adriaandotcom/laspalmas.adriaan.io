45627678e58f8a562ba42979a0a3df47926ce80c
Adriaan <harianus@users.noreply.github.com>
2016-03-15 22:49:27 +0100

    Make it https

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ document.querySelector('form.newsletter').addEventListener('submit', function(ev
   var email = document.querySelector('form.newsletter input[name="email"]').value
   
   // Send newsletter email to myself
-  var url = 'http://watiseropderadio.nl/tools/mail?subject=New+subscriber+Las+Palmas+blog:+' + email;
+  var url = 'https://screenmessage.com/blog/mail.php?subject=New+subscription+for+Las+Palmas+blog&message=Email+address:+' + email;
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
   request.send();
